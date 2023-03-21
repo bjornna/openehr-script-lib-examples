dbe780df1ee395807eb794d7eee475acc0764831
Bjoern Naess <bna@dips.no>
2023-03-21 22:13:30 +0100

    Added more tests and some doc

--- duration-week-year-calcs/src/weekyear.ts
+++ duration-week-year-calcs/src/weekyear.ts
@@ -2,18 +2,19 @@
  * 
  * @param d1 first date
  * @param d2 last date
- * @returns the duration between the dates in years and weeks 
+ * @returns the absolute duration between the dates in years and weeks (will never be negative)
  */
 export function getYearWeekDurationBetween(d1: Date, d2: Date): YearWeekDuration {
+    // we will use the absolute difference between the two dates 
     let delta = Math.abs(d2.getTime() - d1.getTime());
-
+    // years is calculated as whole years 
     const years = Math.floor(delta / msInYear);
+    // subtract the whole years from the msDelta
     delta -= years * msInYear;
-
+    // the rest gives number of weeks and we want it modulo ~ 52 
     const weeks = Math.floor(delta / msInWeek) % NominalWeeksInYear;
-
+    // sometime the result will be 52 weeks - then add one to year and set weeks to zero 
     if (52 == weeks) {
-        // sometime the result will be 52 weeks - then add one to year and set weeks to zero 
         return {
             years: years + 1,
             weeks: 0
--- duration-week-year-calcs/test/duration.spec.ts
+++ duration-week-year-calcs/test/duration.spec.ts
@@ -50,8 +50,8 @@ describe("Verify dates", () => {
         }
     ];
     suite.forEach(testCase => {
-        it(toTestString(testCase), () => {            
-            const r = getYearWeekDurationBetween(parseDate(testCase.date1),parseDate(testCase.date2));
+        it(toTestString(testCase), () => {
+            const r = getYearWeekDurationBetween(parseDate(testCase.date1), parseDate(testCase.date2));
             expect(r.weeks, "Weeks ").equal(testCase.weeks);
             expect(r.years, "Years ").equal(testCase.years);
         })
@@ -59,6 +59,39 @@ describe("Verify dates", () => {
 
 
 })
+describe("Test days <= 7 days", () => {
+    let n = 7;
+    while (n > 0) {
+        const base = "2023.03.01";
+        const d2 = `2023.03.0${n}`;
+        const d3 = `202${n}.03.01`;
+        it("Days = " + d2, () => {
+            const diff = getYearWeekDurationBetween(parseDate(base), parseDate(d2));
+            expect(diff.weeks).equal(0);
+            expect(diff.years).equal(0);
+        })
+        it(`Years ${d3}`,()=>{
+            const diff = getYearWeekDurationBetween(parseDate(base), parseDate(d3));
+            expect(diff.weeks).equal(0);
+        })
+        n--;
+    }
+})
+describe("Test years on same date", ()=>{
+    let year = 1980;
+    while(year <= 2023){
+        const d2 = "2023.02.01";
+        const d1 = `${year}.02.01`;
+        it(`${d2} should be weeks = 0`, ()=>{
+            const diff = getYearWeekDurationBetween(parseDate(d1), parseDate(d2));
+            expect(diff.weeks).eq(0);
+        })
+        year++;
+
+    }
+})
+
+
 
 describe("Test suite day by day", () => {
     const base = "2023-01-01";
