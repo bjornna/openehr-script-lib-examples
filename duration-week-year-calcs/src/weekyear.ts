b92d5ac4ab3b9d1cace6ac9ebdf7e7c8b4cfcd4d
Bjoern Naess <bna@dips.no>
2023-03-21 19:33:53 +0100

    Refactored code

--- duration-week-year-calcs/src/weekyear.ts
+++ duration-week-year-calcs/src/weekyear.ts
@@ -1,27 +1,9 @@
-
-
-const NominalDaysInYear = 365.242199;
-const NominalDaysInMonth = NominalDaysInYear / 12.0;
-const SecondsInMinute = 60;
-const MinutesInHour = 60;
-const HoursInDay = 24;
-const MaxDaysInMonth = 31;
-const DaysInYear = 365;
-const DaysInLeapYear = 366;
-const MaxDaysInYear = DaysInLeapYear;
-const DaysInWeek = 7;
-const MonthsInYear = 12;
-const MilliSecondsInSeconds = 1000;
-const MilliSecondsInDay = MilliSecondsInSeconds * SecondsInMinute * MinutesInHour * HoursInDay;
-const NominalWeeksInYear = 52.177457;
-
-const msInYear = NominalDaysInYear * HoursInDay * MinutesInHour * SecondsInMinute * MilliSecondsInSeconds;
-const msInWeek = DaysInWeek * HoursInDay * MinutesInHour * SecondsInMinute * MilliSecondsInSeconds;
-
-export type YearWeekDuration = {
-    years?: number;
-    weeks?: number;
-}
+/**
+ * 
+ * @param d1 first date
+ * @param d2 last date
+ * @returns the duration between the dates in years and weeks 
+ */
 export function getYearWeekDurationBetween(d1: Date, d2: Date): YearWeekDuration {
     let delta = Math.abs(d2.getTime() - d1.getTime());
 
@@ -30,18 +12,43 @@ export function getYearWeekDurationBetween(d1: Date, d2: Date): YearWeekDuration
 
     const weeks = Math.floor(delta / msInWeek) % NominalWeeksInYear;
 
-
     if (52 == weeks) {
+        // sometime the result will be 52 weeks - then add one to year and set weeks to zero 
         return {
             years: years + 1,
             weeks: 0
         }
     } else {
         return {
-            years, weeks
+            years,
+            weeks
         }
     }
-
-
 }
 
+// openEHR constants for time related variables 
+const NominalDaysInYear = 365.242199;
+//const NominalDaysInMonth = NominalDaysInYear / 12.0;
+const SecondsInMinute = 60;
+const MinutesInHour = 60;
+const HoursInDay = 24;
+//const MaxDaysInMonth = 31;
+//const DaysInYear = 365;
+//const DaysInLeapYear = 366;
+//const MaxDaysInYear = DaysInLeapYear;
+const DaysInWeek = 7;
+//const MonthsInYear = 12;
+const MilliSecondsInSeconds = 1000;
+//const MilliSecondsInDay = MilliSecondsInSeconds * SecondsInMinute * MinutesInHour * HoursInDay;
+const NominalWeeksInYear = 52.177457;
+
+const msInYear = NominalDaysInYear * HoursInDay * MinutesInHour * SecondsInMinute * MilliSecondsInSeconds;
+const msInWeek = DaysInWeek * HoursInDay * MinutesInHour * SecondsInMinute * MilliSecondsInSeconds;
+
+/**
+ * A type to carry duration data 
+ */
+export type YearWeekDuration = {
+    years?: number;
+    weeks?: number;
+}
